4451562a7fd68ade2a7c07a2389a6fd7a93f307f
mpoosang <mpoosang@gmail.com>
2020-12-21 18:48:05 -0800

    fixed error messages

--- src/components/Customize.js
+++ src/components/Customize.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from 'react'
 import Specs from './Specs'
 
 export default function Customize(props) {
--- src/components/Specs.js
+++ src/components/Specs.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Parts from './Parts'
 
 export default function Specs(props) {
